18cb3d065523188a8bf261b86284fd7a66b058b7
Txinto Vaz <txinto.vaz@gtc.iac.es>
2022-05-23 12:08:58 +0100

    Adding safety checks

--- porispanel_csys.sh
+++ porispanel_csys.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1;
+fi
+
+FILE=models/$1.graphml
+if test -f "$FILE"; then
+    echo "Input $FILE exists, continuing"
+else
+    echo "Input $FILE does not exist, aborting"
+    exit 1;
+fi
 rm models/$1.ods
 rm models/$1.xml
 cp pyPORIS/config_rm_enabled.py pyPORIS/config_rm.py || { echo "config_rm_enabled.py missing"; exit 1; } 
